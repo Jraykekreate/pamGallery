c0af258b04e66d7bd13d5224abc56ed4eab34198
Xaje <jnerdhiper@gmail.com>
2024-06-07 17:26:10 +0000

    minor name update

--- pamGallery/src/pages/SignUpLogin.tsx
+++ pamGallery/src/pages/SignUpLogin.tsx
@@ -32,7 +32,7 @@ export default function Login () {
                 
                 {signUpLoginState === 'Login' ?  "" :  <input className="p-2 py-2 rounded-xl border" type="name" name="userName" placeholder="Enter a username"></input> }
                 {signUpLoginState === 'Login' ?  "" :  <input className="p-2 py-2 rounded-xl border" type="name" name="firstName" placeholder="Enter A First Name"></input> }
-                {signUpLoginState === 'Login' ?  "" :  <input className="p-2  py-2 rounded-xl border" type="name" name="lastName" placeholder="Enter a Last"></input> }
+                {signUpLoginState === 'Login' ?  "" :  <input className="p-2  py-2 rounded-xl border" type="name" name="lastName" placeholder="Enter a Last Name"></input> }
 
                 <input className="p-2 rounded-xl border" type="email" name="email" placeholder="Email"></input>
                 <div className="relative">
