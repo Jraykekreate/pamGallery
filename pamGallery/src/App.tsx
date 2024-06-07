2c7863d4aea4d4e9a43ecf7e241467eec19abdb9
Xaje <jnerdhiper@gmail.com>
2024-06-07 06:39:47 +0000

    changed some lines for it to compile on netlify

--- pamGallery/src/App.tsx
+++ pamGallery/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
 
   return (
